babd00b23631b0d1a1cf35c77c601fda5860a2e3
Mateusz Judka <mrjudka@gmail.coM>
2024-05-02 23:05:42 +0100

    Generates 1-100, 1 and 100 are changable

---
+++ EurofinsCodingTask/wwwroot/js/numberDisplayScript.js
@@ -0,0 +1,58 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.getElementById("btnAddText").addEventListener("click", function () {
+    var text = document.createElement("p");
+    text.textContent = "This is a new text";
+    document.getElementById("textContainer").appendChild(text);
+});
+
+document.getElementById("btnDeleteAndRefresh").addEventListener("click", function () {
+    document.getElementById("textContainer").innerHTML = "";
+
+
+});
+
+//display 1-100
+document.getElementById("btnUseFunction").addEventListener("click", function () {
+
+    console.log("btnUseFunction clicked");
+    document.getElementById("textContainer").innerHTML = ""; // Clear previous entries
+
+    var customValue1 = document.getElementById("customValue1").value || "1"; // Default if empty
+    var customValue100 = document.getElementById("customValue100").value || "100"; // Default if empty
+
+    for (var i = 1; i <= 100; i++) {
+        var text = document.createElement("p");
+        text.setAttribute("id", i);
+
+        if (i === 1) {
+            text.textContent = customValue1;
+        }
+        else if (i === 100) {
+            text.textContent = customValue100;
+        }
+
+        //if statement multiples of both 3 and 5 display “Eurofins”.
+        else if (i % 3 == 0 && i % 5 == 0) {
+            text.textContent = "Eurofins";
+        }
+        //if statement when a number is divisable by 5 display "Five"
+        else if (i % 5 == 0) {
+            text.textContent = "Five";
+        }
+        //if statement when a number is divisable by 3 display "Three"
+        else if (i % 3 == 0 ) {
+            text.textContent = "Three";
+        }
+        else {
+            text.textContent = i;
+        }
+        document.getElementById("textContainer").appendChild(text);
+    }
+    
+
+
+});
