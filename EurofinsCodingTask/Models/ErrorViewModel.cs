399476fec5218e0c94a8a6f38546e9be796297e9
Mateusz Judka <mrjudka@gmail.coM>
2024-05-02 21:40:20 +0100

    Add project files.

---
+++ EurofinsCodingTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EurofinsCodingTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
