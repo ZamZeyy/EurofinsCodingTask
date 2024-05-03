e753dcf067560b0ff171f93ddfb7d2d4c5bf60b7
Mateusz Judka <mrjudka@gmail.coM>
2024-05-03 23:42:27 +0100

    changing backgrount of the boxes + css styling

--- EurofinsCodingTask/wwwroot/js/numberDisplayScript.js
+++ EurofinsCodingTask/wwwroot/js/numberDisplayScript.js
@@ -8,8 +8,18 @@
 
 
 //display 1-100
-document.getElementById("btnUseFunction").addEventListener("click", function () {
 
+var buttonClicked = false;
+document.getElementById("btnUseFunction").addEventListener("click", function () {
+    buttonClicked = true;
+    ButtonClick();
+    
+
+
+});
+
+
+function ButtonClick() {
     console.log("btnUseFunction clicked");
     document.getElementById("textContainer").innerHTML = ""; // Clear previous entries
 
@@ -18,7 +28,7 @@ document.getElementById("btnUseFunction").addEventListener("click", function () 
 
     for (var i = 1; i <= 100; i++) {
         var text = document.createElement("p");
-        
+
 
         if (i === 1) {
             text.textContent = customValue1;
@@ -30,27 +40,67 @@ document.getElementById("btnUseFunction").addEventListener("click", function () 
         //if statement multiples of both 3 and 5 display “Eurofins”.
         else if (i % 3 == 0 && i % 5 == 0) {
             text.textContent = "Eurofins";
-            
+                if (orangeToggle) {
+                    text.className = "orange-background";
+                }
+
         }
         //if statement when a number is divisable by 5 display "Five"
         else if (i % 5 == 0) {
             text.textContent = "Five";
-            
+                if (redToggle) {
+                    text.className = "red-background";
+                }
 
         }
         //if statement when a number is divisable by 3 display "Three"
-        else if (i % 3 == 0 ) {
+        else if (i % 3 == 0) {
             text.textContent = "Three";
-            
+                if (blueToggle) {
+                    text.className = "blue-background";
+                }
+
         }
         else {
             text.textContent = i;
         }
         document.getElementById("textContainer").appendChild(text);
     }
+
+}
+
+
+//if type="checkbox" id="toggleColorBlue" is checked, set blueToggle to True
+var blueToggle = false;
+document.getElementById("toggleColorBlue").addEventListener("change", function () {
+    blueToggle = this.checked;
+    console.log("blueToggle: " + blueToggle);
+    if (buttonClicked == true){
+        ButtonClick();
+    }
     
+});
 
 
+
+//if type="checkbox" id="toggleColorRed" is checked, set redToggle to True
+var redToggle = false;
+document.getElementById("toggleColorRed").addEventListener("change", function () {
+    redToggle = this.checked;
+    console.log("redToggle: " + redToggle);
+    if (buttonClicked == true) {
+        ButtonClick();
+    }
+});
+
+//if type="checkbox" id="toggleColorOrange" is checked, set orangeToggle to True
+var orangeToggle = false;
+document.getElementById("toggleColorOrange").addEventListener("change", function () {
+    orangeToggle = this.checked;
+    console.log("orangeToggle: " + orangeToggle);
+    if (buttonClicked == true) {
+        ButtonClick();
+    }
 });
 
 
